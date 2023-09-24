b505902ee5b721017d19f63ee7dc55470152a97c
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-09-24 20:11:07 +0100

    add custon signup flow

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-    <h1>Home</h1>
+      <h1 className='text-2xl font-bold mb-5'>Home</h1>
+      <p className='mb-5'>
+        Testing Out Auth and Custom Authentication with Clerk
+      </p>
+      <p className='mb-5'>
+        Go ahead to SignUp and SignIn to explore features
+      </p>
     </>
   )
 }
